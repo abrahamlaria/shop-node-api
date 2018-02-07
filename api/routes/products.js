764267213e5391f542494db39c3409cca60bc4df
Abraham Laria <alaria@LSCMC024-315.local>
2018-02-07 11:23:07 -0800

    Added routes to GET, PATCH and DELETE a single product

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
+// All products
 router.get('/', (req, res, next) => {
     res.status(200).json({
         message: 'Handling GET requests to /products'
@@ -10,6 +11,33 @@ router.get('/', (req, res, next) => {
 router.post('/', (req, res, next) => {
     res.status(200).json({
         message: 'Handling POST requests to /products'
+    });
+});
+
+//Single product
+router.get('/:productId', (req, res, next) => {
+    const id = req.params.productId;
+    if(id === 'special') {
+         res.status(200).json({
+             message: 'You discovered the special ID',
+             id: id
+         });
+    } else {
+        res.status(200).json({
+            message: 'You passed an ID'
+        });
+    }
+});
+
+router.patch('/:productId', (req, res, next) => {
+    res.status(200).json({
+        message: 'Updated product!'
+    });
+});
+
+router.delete('/:productId', (req, res, next) => {
+    res.status(200).json({
+        message: 'Deleted product!'
     });
 });
 
