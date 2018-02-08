efe27b6aa2f674d838768ed01e6b9a12f8a2a763
Abraham Laria <alaria@LSCMC024-315.local>
2018-02-08 09:34:35 -0800

    Added comment

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(bodyParser.urlencoded({
 //Extract json data and makes it readable
 app.use(bodyParser.json());
 
-//Adding headers to handle CORS blah blah blah
+//Adding headers to handle CORS
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header(
