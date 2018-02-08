fb4f2f889d48911632a2be1c519746215d732978
Abraham Laria <alaria@LSCMC024-315.local>
2018-02-08 08:33:01 -0800

    Added mongoose schema for a product

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
-const mogoose = require('mongoose');
+const mongoose = require('mongoose');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
