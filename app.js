69bdcb0e30cdafb2e4e655689e0caddc2277395b
Abraham Laria <alaria@LSCMC024-315.local>
2018-02-09 13:37:54 -0800

    Hashed password. Finished the user route.

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const mongoose = require('mongoose');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
+const userRoutes = require('./api/routes/user');
 
 mongoose.connect('mongodb://nodeshop:' + process.env.MONGO_ATLAS_PW + '@node-rest-shop-shard-00-00-eotmg.mongodb.net:27017,node-rest-shop-shard-00-01-eotmg.mongodb.net:27017,node-rest-shop-shard-00-02-eotmg.mongodb.net:27017/test?ssl=true&replicaSet=node-rest-shop-shard-0&authSource=admin');
 mongoose.Promise = global.Promise;
@@ -44,6 +45,7 @@ app.use((req, res, next) => {
 //Routes which should handle requests
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
+app.use('/user', userRoutes);
 
 //Handles not found routes
 app.use((req, res, next) => {
